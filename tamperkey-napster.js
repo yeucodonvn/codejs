b24e013e4c284606fb3ff6677ce7088872e70211
yeucodonvn <yeucodonvn@gmail.com>
2019-11-22 09:26:41 +0700

    Update tamperkey-napster.js

--- tamperkey-napster.js
+++ tamperkey-napster.js
@@ -1,7 +1,7 @@
 
 // ==UserScript==
 // @name         Naspter
-// @version      0.2.2
+// @version      0.2.3
 // @description  This script Autoplay Naspter
 // @author       yeucodon
 // @updateURL    https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-napster.js
@@ -19,26 +19,8 @@
     var BEFORE_DURATION = 80;//                     will increase from 1-20;
     var REPEAT_NUMB = 200;//                          will increase from 1-5;
     var ADDED_EVENT = 0;
-    var CORRECT_ARTIST = true;
-	var play_loading = true;
-
-
-
-	function setShuffle(toggle){
-        var ShuffleElm = document.querySelector("#ember57");
-        var checked = ShuffleElm.getAttribute("class");
-        if(toggle == (checked=="shuffle-button icon-shuffle2 active ember-view"?false:true)){
-            ShuffleElm.click();
-        }
-    };
-
-    function setAutoPlay(toggle){
-        var autoPlayElm = document.getElementById("ember53");
-        var checked = autoPlayElm.getAttribute("title");
-        if(toggle == (checked=="Repeat On"?false:true)){
-            autoPlayElm.click();
-        }
-    };
+    
+	
 
 
 
@@ -46,9 +28,6 @@
         var player = document.querySelector('[title="Next track"]');
 		player.click();
         if(ADDED_EVENT!==1){
-            player.addEventListener("onStateChange", function(state){
-                if(state === 0){
-                    
                         console.log(REPEAT_NUMB);
                         if(REPEAT_NUMB > 0){
                             setTimeout(seekSliderBar,(Math.floor(Math.random() * 20) + BEFORE_DURATION)*1000);
@@ -56,19 +35,14 @@
                             location.reload();
                         }
                         REPEAT_NUMB--;
-                    
-                }
-            });
-
+                };
             ADDED_EVENT = 1;
         }
     };
 
     function run(){
         console.log("Naspter AutoPlay - MANAGER");
-
-		setAutoPlay(true);console.log("Naspter setAutoPlay");
-		setShuffle(true);console.log("Naspter setShuffle");
+		
         setTimeout(seekSliderBar,(Math.floor(Math.random() * 20) + BEFORE_DURATION)*1000);
     };
 

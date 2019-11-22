ea733a3b185b36f9c61830fbfd2d68b891141277
yeucodonvn <yeucodonvn@gmail.com>
2019-11-22 08:57:00 +0700

    Update tamperkey-napster.js

--- tamperkey-napster.js
+++ tamperkey-napster.js
@@ -1,7 +1,7 @@
 
 // ==UserScript==
 // @name         Naspter
-// @version      0.2.1
+// @version      0.2.2
 // @description  This script Autoplay Naspter
 // @author       yeucodon
 // @updateURL    https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-napster.js
@@ -15,22 +15,14 @@
 (function() {
     'use strict';
 
-    var ARTIST_ID = "PL_2SVRWG1wuP31KNu4sQPvtmL2Fc5JcZA";
-    var BEFORE_DURATION = 60;//                     will increase from 1-20;
-    var REPEAT_NUMB = 20;//                          will increase from 1-5;
+    
+    var BEFORE_DURATION = 80;//                     will increase from 1-20;
+    var REPEAT_NUMB = 200;//                          will increase from 1-5;
     var ADDED_EVENT = 0;
     var CORRECT_ARTIST = true;
 	var play_loading = true;
 
-	//check pasue button loading
-	//document.querySelector("#player-transport-controls > div.transport-top > div.player-play-button > div.player-loading")
-	//class="player-loading active"
-	function play_button(){
-        var playElm = document.querySelector("#player-transport-controls > div.transport-top > div.player-play-button > div.player-loading");
-        var checked = playElm.getAttribute("class");
-		play_loading="player-loading"?true:false;
-        
-    };
+
 
 	function setShuffle(toggle){
         var ShuffleElm = document.querySelector("#ember57");
@@ -46,40 +38,40 @@
         if(toggle == (checked=="Repeat On"?false:true)){
             autoPlayElm.click();
         }
-		//else{
-		//	setAutoPlay(true);
-		//}
     };
 
 
-		function setRandomInterval(f, min, max) {
-			setTimeout(function() {
-				f();
-				if(ADDED_EVENT!==1){
-					console.log("Check Loanding");
-					setRandomInterval(play_button(), 20000);
-					if(REPEAT_NUMB > 0 && play_loading){
-						setRandomInterval(function(){document.querySelector('[title="Next track"]').click();}, 68000, 108000);
 
-					}else{
-                        location.reload();
-                     }
+	function seekSliderBar(){
+        var player = document.querySelector('[title="Next track"]');
+		player.click();
+        if(ADDED_EVENT!==1){
+            player.addEventListener("onStateChange", function(state){
+                if(state === 0){
+                    
+                        console.log(REPEAT_NUMB);
+                        if(REPEAT_NUMB > 0){
+                            setTimeout(seekSliderBar,(Math.floor(Math.random() * 20) + BEFORE_DURATION)*1000);
+                        }else{
+                            location.reload();
+                        }
                         REPEAT_NUMB--;
-				}
-				ADDED_EVENT = 1;
-			}, min + Math.random() * (max - min));
+                    
+                }
+            });
 
-		};
-
+            ADDED_EVENT = 1;
+        }
+    };
 
     function run(){
         console.log("Naspter AutoPlay - MANAGER");
 
 		setAutoPlay(true);console.log("Naspter setAutoPlay");
 		setShuffle(true);console.log("Naspter setShuffle");
-        setRandomInterval(function(){document.querySelector('[title="Next track"]').click();}, 88000, 128000);
+        setTimeout(seekSliderBar,(Math.floor(Math.random() * 20) + BEFORE_DURATION)*1000);
     };
 
-    setTimeout(run, 10000);
+    setTimeout(run, 5000);
 })();
 

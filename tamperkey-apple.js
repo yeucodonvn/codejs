77a15489dbd2297bfe824d9bdce8cedf4f480214
yeucodonvn <yeucodonvn@gmail.com>
2019-12-05 13:36:04 +0700

    Update tamperkey-apple.js

--- tamperkey-apple.js
+++ tamperkey-apple.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         apple
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.4
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @updateURL    https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-apple.js
 // @downloadURL  https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-apple.js
@@ -18,5 +18,6 @@
     setRandomInterval(f, min, max)
   }, min + Math.random() * (max - min));
 };
-setRandomInterval(function(){document.querySelector('[aria-label="Next"]').click()}, 880000, 1200000);
+    setRandomInterval(function(){document.querySelector("#ember21 > div > div.web-chrome-playback-controls__main > button:nth-child(3)").click()}, 880000, 1200000);
+//setRandomInterval(function(){document.querySelector('[aria-label="Next"]').click()}, 880000, 1200000);
 })();

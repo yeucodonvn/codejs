c551aa0d14ef5a4daaa9bdfb204416ed1477597e
yeucodonvn <yeucodonvn@gmail.com>
2020-02-25 09:21:03 +0700

    Update tamperkey-deezer.js

--- tamperkey-deezer.js
+++ tamperkey-deezer.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         deezer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @version      0.2
+// @require  	 https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @updateURL    https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-deezer.js
 // @downloadURL  https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-deezer.js
 // @description  try to take over the world!
@@ -12,12 +12,46 @@
 // ==/UserScript==
 
 (function() {
-function setRandomInterval(f, min, max) {
-  setTimeout(function() {
-    f();
-    setRandomInterval(f, min, max)
-  }, min + Math.random() * (max - min));
-};
-setRandomInterval(function(){document.querySelector("[aria-label='Next']").click();}, 68000, 88000);
+function play(){
+	 console.log("play");
+	var play = document.querySelector("[aria-label='Next']").click();
+	setTimeout(pause,100*60*1000);
+}
+
+function Shuffle(){
+	 console.log("Shuffle");
+	var Shuffle= document.querySelector("[aria-label='Turn on Shuffle']")!==null;
+	if(Shuffle)	{
+		document.querySelector("[aria-label='Turn on Shuffle']").click();
+	}
+}
+function pause(){
+	 console.log("pause");
+	var repeatElm = document.querySelector("[class='svg-icon-group-btn is-highlight']");
+	var repeatLabel = repeatElm.getAttribute("aria-label");
+	if(repeatLabel == "Pause"){
+	repeatElm.click();
+		setTimeout(reload,10*1000);
+	}
+}
+
+function reload(){
+	 console.log("reload");
+	var repeatElm = document.querySelector("[class='svg-icon-group-btn is-highlight']");
+	var repeatLabel = repeatElm.getAttribute("aria-label");
+	if(repeatLabel == "Play"){
+		window.location.reload();
+	}
+}
+
+function run() {
+        console.log("Deezer");
+
+        $(window).off('beforeunload.windowReload');
+		Shuffle();
+       	setTimeout(play,10*1000);
+    };
+
+    setTimeout(run, 5000);
 
 })();

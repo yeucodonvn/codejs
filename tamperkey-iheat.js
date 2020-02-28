e08e97a808b2524a3bf4cbca727f6e800f331031
yeucodonvn <yeucodonvn@gmail.com>
2020-02-28 21:48:03 +0700

    Update tamperkey-iheat.js

--- tamperkey-iheat.js
+++ tamperkey-iheat.js
@@ -1,19 +1,44 @@
 // ==UserScript==
 // @name         iheart
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @version      0.4
+// @require 	 https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-iheat.js
 // @downloadURL  https://raw.githubusercontent.com/yeucodonvn/codejs/master/tamperkey-iheat.js
 // @match        https://www.iheart.com/playlist/*
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
 
     var REPEAT_NUMB = 200;
+
+	function play_btn(){
+		console.log("play btn");
+		var element = document.querySelector('[data-test="play-button"]');
+		console.log(element);
+		var loopClickRepeat = setInterval(function(){
+            if(element !== null){
+                var repeatLabel = element.getAttribute("data-test-state");
+                if(repeatLabel == "paused"){
+					console.log("click play btn");
+                    element.click();
+					setRandomInterval(function(){document.querySelector('[data-test="skip-button"]').click()}, 88000, 128000);
+                }else{
+					console.log("search play btn");
+                    clearInterval(loopClickRepeat);
+                }
+			}
+			else{
+				console.log("search element");
+				clearInterval(loopClickRepeat);
+				setTimeout(play_btn, 2000);
+			}
+        }, 2000);
+	};
 
    function setRandomInterval(f, min, max) {
 			if(REPEAT_NUMB>0){
@@ -24,17 +49,11 @@
 			} else {location.reload();}
 				REPEAT_NUMB--;
 	};
-	
-	function play_btn(){
-	var element = document.querySelector('[data-test="play-button"]');
-		element.click();
-	}
-	
+
 	function run() {
         console.log("IHEAT AutoPlay - MANAGER");
 		play_btn();
-
-        setRandomInterval(function(){document.querySelector('[data-test="skip-button"]').click()}, 88000, 128000);
+		//setTimeout(play_btn, 5000);
     };
 
     setTimeout(run, 5000);

58307747c78cc7995e0382c67b2a2b6072ee44d6
yeucodonvn <yeucodonvn@gmail.com>
2020-03-02 10:04:10 +0700

    Update tamperkey-iheat.js

--- tamperkey-iheat.js
+++ tamperkey-iheat.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iheart
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.4.1
 // @require 	 https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @description  try to take over the world!
 // @author       You
@@ -37,7 +37,17 @@
                 if(repeatLabel == "paused"){
 					console.log("click play btn");
                     element.click();
-					setRandomInterval(function(){document.querySelector('[data-test="skip-button"]').click()}, 88000, 128000);
+
+
+					var loopGetDuration_First = setInterval(function(){
+					var totalDuration = hmsToSecondsOnly(document.querySelector('[data-test="player-total-time"]').textContent.trim());
+					if(totalDuration>0){
+						console.log("Get duration Total "+totalDuration);
+						clearInterval(loopGetDuration_First);
+						setTimeout(get_time,(totalDuration-3)*1000);
+					}
+					}
+				)
                 }else{
 					console.log("search play btn");
                     clearInterval(loopClickRepeat);
@@ -50,22 +60,63 @@
 			}
         }, 2000);
 	};
+    function hmsToSecondsOnly(str) {
+        var p = str.split(':'),
+            s = 0, m = 1;
 
-   function setRandomInterval(f, min, max) {
-			if(PARAMS.REPEAT_NUMB_IHEART>0){
-				console.log("next");
-				setTimeout(function() {
-					f();
-					setRandomInterval(f, min, max)
-				}, min + Math.random() * (max - min));
+        while (p.length > 0) {
+            s += m * parseInt(p.pop(), 10);
+            m *= 60;
+        }
+
+        return s;
+    };
+
+	function searchconfirm(){
+		var confirmdlg = document.querySelector('[class="confirm-dialog dialog-container"]');
+        if(confirmdlg != null){
+			var ok_btn = document.querySelector('[data-test="confirm-change-button"]');
+			ok_btn.click();
+			console.log("Click Confirm");
+        }
+    };
+	var temp_number = 0;
+	function get_time() {
+			if(temp_number>0){
+				console.log(temp_number);
+				var loopGetDuration_First = setInterval(function(){
+					var totalDuration = hmsToSecondsOnly(document.querySelector('[data-test="player-total-time"]').textContent.trim());
+					if(totalDuration>0){
+						console.log("Get duration Total "+totalDuration);
+						clearInterval(loopGetDuration_First);
+						temp_number--;
+						setTimeout(get_time,(totalDuration-3)*1000);
+					}
+					}
+				)
 			} else {location.reload();}
-				PARAMS.REPEAT_NUMB_IHEART--;
 	};
+	function get_loading(){
+		var totalDuration = hmsToSecondsOnly(document.querySelector('[data-test="player-total-time"]').textContent.trim());
+		var current_time = hmsToSecondsOnly(document.querySelector('[data-test="player-current-time"]').textContent.trim());
+		if(current_time==0&&totalDuration==0){
+			console.log("loading error, click next ");
+			document.querySelector('[data-test="skip-button"]').click();
+			get_time();
+		}
+	};
+	function shuffle(){
+		document.querySelector('[data-test="shuffle"]').click();
+	};
+	
 
 	function run() {
         console.log("IHEAT AutoPlay - MANAGER - Repeat Number "+PARAMS.REPEAT_NUMB_IHEART);
+		temp_number=PARAMS.REPEAT_NUMB_IHEART;
 		play_btn();
-		//setTimeout(play_btn, 5000);
+		setTimeout(shuffle, 15000);
+		setInterval(searchconfirm,25*60*1000);
+		setInterval(get_loading,50*1000);
     };
 
     setTimeout(run, 5000);

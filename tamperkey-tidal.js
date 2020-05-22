75fd972bd7053905d0a2fbe16a15d9b015eca588
yeucodonvn <yeucodonvn@gmail.com>
2020-05-23 06:26:11 +0700

    Update tamperkey-tidal.js

--- tamperkey-tidal.js
+++ tamperkey-tidal.js
@@ -1,7 +1,6 @@
-
 // ==UserScript==
 // @name         Tidal
-// @version      0.2.4
+// @version      0.2.5
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @description  This script Autoplay Tidal
 // @author       yeucodon
@@ -16,7 +15,7 @@
 (function() {
     'use strict';
 
-    var REPEAT_NUMB = 200;//                          will increase from 1-5;
+    var REPEAT_NUMB = 200;        
 
    function setRandomInterval(f, min, max) {
 			if(REPEAT_NUMB>0){
@@ -27,5 +26,29 @@
 			} else {location.reload();}
 				REPEAT_NUMB--;
 	};
-setRandomInterval(function(){document.querySelector('[title="Next"]').click()}, 88000, 128000);
+	function clickshuffle(){	
+	        console.log("click shuffleAll");
+
+			document.querySelector("[data-track--button-id='shuffleAll'][data-track--content-type='playlist']").click();
+	};
+	function search_footer_player(){	
+	        console.log("search footer player");
+		var searchft=document.querySelector("[data-test='footer-player'][data-track--module-id='footer_player']");
+		if(searchft==null){
+			clickshuffle();
+			}
+		
+	};
+	
+	function run() {
+        console.log("Tidal AutoPlay - MANAGER");
+
+        $(window).off('beforeunload.windowReload');
+       	setTimeout(clickshuffle,10*1000);
+		setInterval(search_footer_player,50*1000);
+        setRandomInterval(function(){document.querySelector('[title="Next"]').click()}, 88000, 128000);
+    };
+
+    setTimeout(run, 5000);
+
 })();

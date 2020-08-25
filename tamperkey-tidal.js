04a627d8285bf529bf0274f387ca5c9b8f276994
yeucodonvn <yeucodonvn@gmail.com>
2020-08-25 12:13:35 +0700

    Update tamperkey-tidal.js

--- tamperkey-tidal.js
+++ tamperkey-tidal.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tidal
-// @version      0.2.4
+// @version      0.3
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @description  This script Autoplay Tidal
 // @author       yeucodon

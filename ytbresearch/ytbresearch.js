dcc036bce844bc5aebfe0a17a83fdf0a1929af63
yeucodonvn <yeucodonvn@gmail.com>
2024-03-18 00:01:05 +0700

    Update ytbresearch.js

--- ytbresearch/ytbresearch.js
+++ ytbresearch/ytbresearch.js
@@ -4,54 +4,69 @@ const Listurl = [];
 const listoutput = [];
 let time = 'd|7';
 let viewpoint = 20000;
-let subpoint = 'n|10000'
+let subpoint = 'l|1000'
 // https://www.magetop.com/blog/cach-lay-api-key-youtube/
 // ytb API key AIzaSyAbBIyID9O1HqSqpt-09aR1VrtH3vBHY7E
 async function videoinfo(videoid = 'kf0Pzw7cNO8') {
-  let response = await fetch(
-    `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoid}&key=${API_KEY}`
-  );
-  const responseText = await response.text();
-  const res = JSON.parse(responseText)
-  let viewCount = parseInt(res.items[0].statistics.viewCount);
-  let likeCount = parseInt(res.items[0].statistics.likeCount);
-  let published = res.items[0].snippet.publishedAt;
-  let channelId = res.items[0].snippet.channelId
-  return { viewCount, likeCount, published, channelId };
+  try {
+    let response = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoid}&key=${API_KEY}`
+    );
+    const responseText = await response.text();
+    const res = JSON.parse(responseText)
+    let viewCount = parseInt(res.items[0].statistics.viewCount);
+    let likeCount = parseInt(res.items[0].statistics.likeCount);
+    let published = res.items[0].snippet.publishedAt;
+    let channelId = res.items[0].snippet.channelId
+    return { viewCount, likeCount, published, channelId };
+  } catch (error) {
+    console.log('Error:', error.stack || error.meessage || error);
+  }
+  return null;
+
 }
 
 async function getChannelSubscriberCount(channelId) {
-  const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${API_KEY}`
-  );
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${API_KEY}`
+    );
+    const data = await response.json();
 
-  if (data.items && data.items.length > 0) {
-    return data.items[0].statistics.subscriberCount;
-  } else {
-    return null;
+    if (data.items && data.items.length > 0) {
+      return data.items[0].statistics.subscriberCount;
+    }
+  } catch (error) {
+    console.log('Error:', error.stack || error.meessage || error);
   }
+  return null;
 };
 
 const infovideo = async (element, eleconfig = {}) => {
-  let videoid = element.querySelector(eleconfig.videoid)?.href?.split('v=')[1]?.split('&')[0] ?? eleconfig.videoid;
-  if (Listurl.includes(videoid)) return;
-  Listurl.push(videoid);
-  let { viewCount, likeCount, published, channelId } = await videoinfo(videoid);
-  let channelsub = parseInt(await getChannelSubscriberCount(channelId));
-  let publishedDate = new Date(published);
-  let uploadDate = publishedDate.getDate() + "/" + (publishedDate.getMonth() + 1) + "/" + publishedDate.getFullYear();
-  let metadataline = element.querySelector(eleconfig.metadataline) ?? eleconfig.metadataline;
-  let view = metadataline.querySelectorAll(eleconfig.view) ?? eleconfig.view;
-  view[0].textContent = viewCount.toLocaleString();
-  view[1].textContent = uploadDate + ` Sub: ${channelsub.toLocaleString()}`;
-  let videoTitle = element.querySelector(eleconfig.videoTitle) ?? eleconfig.videoTitle;
+  try {
+    let videoid = element.querySelector(eleconfig.videoid)?.href?.split('v=')[1]?.split('&')[0] ?? eleconfig.videoid;
+    if (Listurl.includes(videoid)) return;
+    Listurl.push(videoid);
+    let { viewCount, likeCount, published, channelId } = await videoinfo(videoid);
+    let channelsub = parseInt(await getChannelSubscriberCount(channelId));
+    let publishedDate = new Date(published);
+    let uploadDate = publishedDate.getDate() + "/" + (publishedDate.getMonth() + 1) + "/" + publishedDate.getFullYear();
+    let metadataline = element.querySelector(eleconfig.metadataline) ?? eleconfig.metadataline;
+    let view = metadataline.querySelectorAll(eleconfig.view) ?? eleconfig.view;
+    view[0].textContent = viewCount.toLocaleString();
+    view[1].textContent = uploadDate + ` Sub: ${channelsub.toLocaleString()}`;
+    let videoTitle = element.querySelector(eleconfig.videoTitle) ?? eleconfig.videoTitle;
 
-  if (settime(time, publishedDate) && viewCount > viewpoint && setdub(subpoint, channelsub)) {
-    videoTitle.textContent += `view ${viewCount.toLocaleString()} | like ${likeCount.toLocaleString()} | upload ${uploadDate} | Sub: ${channelsub}`; // Thêm thông tin vào title video
+    videoTitle.textContent += `view ${viewCount.toLocaleString()} | like ${likeCount.toLocaleString()} | upload ${uploadDate} | Sub: ${channelsub.toLocaleString()}`; // Thêm thông tin vào title video
     videoTitle.setAttribute('aria-label', videoTitle.textContent);
-    videoTitle.style.color = '#1DAB6F'; // Thay #ff0000 bằng mã màu của bạn
-    listoutput.push(`https://www.youtube.com/watch?v=${videoid} ${viewCount.toLocaleString()} like ${likeCount.toLocaleString()}  upload ${uploadDate}  ${channelId}  Sub: ${channelsub}`);
+    videoTitle.setAttribute('title', videoTitle.textContent);
+    if (settime(time, publishedDate) && viewCount > viewpoint && setdub(subpoint, channelsub)) {
+      videoTitle.style.color = '#1DAB6F'; // Thay #ff0000 bằng mã màu của bạn
+      listoutput.push(`https://www.youtube.com/watch?v=${videoid} ${viewCount.toLocaleString()} like ${likeCount.toLocaleString()}  upload ${uploadDate}  ${channelId}  Sub: ${channelsub.toLocaleString()}`);
+    }
+  } catch (error) {
+    console.log('Error:', error.stack || error.meessage || error);
+
   }
 
 }
@@ -74,7 +89,7 @@ function settime(time, publishedDate) {
 
   switch (query) {
     case 'd':
-      timepoint.setDay(currentDate.getDay() - parseInt(time.split('|')[1]));
+      timepoint.setDate(currentDate.getDay() - parseInt(time.split('|')[1]));
       return publishedDate >= timepoint
     case 'm':
       timepoint.setMonth(currentDate.getMonth() - parseInt(time.split('|')[1]));
@@ -89,19 +104,19 @@ function settime(time, publishedDate) {
 }
 
 async function fistRunWatch() {
-  let secondaryNode = document.querySelector('#secondary-inner');
-  let listvideo = secondaryNode.querySelectorAll('ytd-compact-video-renderer');
+  let secondaryNode = document.querySelector('#secondary-inner,ytd-search');
+  let listvideo = secondaryNode.querySelectorAll('ytd-compact-video-renderer,ytd-video-renderer');
   listvideo.forEach(async (element) => {
     eleconfig = {
-      videoid: '.yt-simple-endpoint.style-scope.ytd-compact-video-renderer',
+      videoid: '.yt-simple-endpoint.style-scope.ytd-compact-video-renderer,a#video-title',
       metadataline: '#metadata-line',
       view: '.inline-metadata-item.ytd-video-meta-block',
-      videoTitle: '#video-title'
+      videoTitle: '#video-title,a#video-title'
     }
     await infovideo(element, eleconfig);
   });
 }
-
+/* 
 async function fistRunSearch() {
   let secondaryNode = document.querySelector('ytd-search');
   let listvideo = secondaryNode.querySelectorAll('ytd-video-renderer');
@@ -114,7 +129,7 @@ async function fistRunSearch() {
     }
     await infovideo(element, eleconfig);
   });
-}
+} */
 
 function mutationSv(partennode, childnode, eleconfig) {
   // Lấy element cần theo dõi
@@ -139,22 +154,28 @@ function mutationSv(partennode, childnode, eleconfig) {
   // Bắt đầu quá trình theo dõi
   observer.observe(targetNode, config);
 }
-async function run(time=0, viewpoint=0, subpoint=0) {
+async function run(time = 0, viewpoint = 0, subpoint = 0) {
   // Thực hiện công việc của hàm run() với các tham số đã nhận
   console.log("Time:", time);
   console.log("Viewpoint:", viewpoint);
   console.log("Subpoint:", subpoint);
 
-  if (window.location.href.includes('watch?v')) {
-      eleconfig = {
-          videoid: '.yt-simple-endpoint.style-scope.ytd-compact-video-renderer',
-          metadataline: '#metadata-line',
-          view: '.inline-metadata-item.ytd-video-meta-block',
-          videoTitle: '#video-title'
-      };
-      mutationSv('#secondary-inner', 'ytd-compact-video-renderer', eleconfig);
-      await fistRunWatch();
-  } else if (window.location.href.includes('results?search_query=')) {
+  // if (window.location.href.includes('watch?v')) {
+  /*  eleconfig = {
+       videoid: '.yt-simple-endpoint.style-scope.ytd-compact-video-renderer',
+       metadataline: '#metadata-line',
+       view: '.inline-metadata-item.ytd-video-meta-block',
+       videoTitle: '#video-title'
+   }; */
+  eleconfig = {
+    videoid: '.yt-simple-endpoint.style-scope.ytd-compact-video-renderer,a#video-title',
+    metadataline: '#metadata-line',
+    view: '.inline-metadata-item.ytd-video-meta-block',
+    videoTitle: '#video-title,a#video-title'
+  }
+  mutationSv('#secondary-inner,ytd-search', 'ytd-compact-video-renderer,ytd-video-renderer', eleconfig);
+  await fistRunWatch();
+  /* } else if (window.location.href.includes('results?search_query=')) {
       eleconfig = {
           videoid: 'a#video-title',
           metadataline: '#metadata-line',
@@ -163,10 +184,10 @@ async function run(time=0, viewpoint=0, subpoint=0) {
       };
       mutationSv('ytd-search', 'ytd-video-renderer', eleconfig);
       await fistRunSearch();
-  }
+  } */
 }
 setTimeout(() => {
   run()
-}, 5*1000);
+}, 5 * 1000);
 // chrome extension
 // https://www.youtube.com/watch?v=Tt2NolG16kQ&list=PLwlNvVIUtWpsjFKGfIXKOz3CjUfg_aQN4&index=2

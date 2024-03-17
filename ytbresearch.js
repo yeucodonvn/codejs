e9454fee75e5895d1eceae55e879572c6dc5dbd5
yeucodonvn <yeucodonvn@gmail.com>
2024-03-17 07:32:32 +0700

    Update ytbresearch.js

--- ytbresearch.js
+++ ytbresearch.js
@@ -3,20 +3,20 @@ const API_KEY = 'AIzaSyAbBIyID9O1HqSqpt-09aR1VrtH3vBHY7E';
 const Listurl = [];
 // https://www.magetop.com/blog/cach-lay-api-key-youtube/
 // ytb API key AIzaSyAbBIyID9O1HqSqpt-09aR1VrtH3vBHY7E
-const videoinfo = async (videoid) => {
+async function videoinfo(videoid = 'kf0Pzw7cNO8') {
   let response = await fetch(
-    `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`
+    `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoid}&key=${API_KEY}`
   );
   const responseText = await response.text();
   const res = JSON.parse(responseText)
-  var viewCount = res.items[0].statistics.viewCount;
-  var likeCount = res.items[0].statistics.likeCount;
-  var published = res.items[0].snippet.publishedAt;
-  var channelId = res.items[0].snippet.channelId
+  let viewCount = parseInt(res.items[0].statistics.viewCount).toLocaleString();
+  let likeCount = parseInt(res.items[0].statistics.likeCount).toLocaleString();
+  let published = res.items[0].snippet.publishedAt;
+  let channelId = res.items[0].snippet.channelId
   return { viewCount, likeCount, published, channelId };
 }
 
-const getChannelSubscriberCount = async (channelId) => {
+async function getChannelSubscriberCount(channelId) {
   const response = await fetch(
     `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${API_KEY}`
   );
@@ -29,53 +29,104 @@ const getChannelSubscriberCount = async (channelId) => {
   }
 };
 
-const infovideo = async (videoid) => {
-  var { viewCount, likeCount, published, channelId } = await videoinfo(videoid);
-  var channelsub = await getChannelSubscriberCount(channelId);
-  // so sánh published định dạng 2023-10-05T04:00:18Z với thời gian hiện tại, lấy ra dạng ngày tháng năm
+const infovideo = async (element, time = 'y|1', eleconfig = {}) => {
+  let videoid = element.querySelector(eleconfig.videoid)?.href?.split('v=')[1] ?? eleconfig.videoid;
+  if (Listurl.includes(videoid)) return;
+  Listurl.push(videoid);
+  let { viewCount, likeCount, published, channelId } = await videoinfo(videoid);
+  let channelsub = parseInt(await getChannelSubscriberCount(channelId)).toLocaleString();
   let publishedDate = new Date(published);
-  let currentDate = new Date();
   let uploadDate = publishedDate.getDate() + "/" + (publishedDate.getMonth() + 1) + "/" + publishedDate.getFullYear();
+  let metadataline = element.querySelector(eleconfig.metadataline) ?? eleconfig.metadataline;
+  let view = metadataline.querySelectorAll(eleconfig.view) ?? eleconfig.view;
+  view[0].textContent = viewCount;
+  view[1].textContent = uploadDate + ` Sub: ${channelsub}`;
+  let videoTitle = element.querySelector(eleconfig.videoTitle) ?? eleconfig.videoTitle;
 
+  if (settime(time, publishedDate)) {
+    videoTitle.textContent += `view ${viewCount} | like ${likeCount} | upload ${uploadDate} | Sub: ${channelsub}`; // Thêm thông tin vào title video
+    videoTitle.setAttribute('aria-label', videoTitle.textContent);
+    videoTitle.style.color = '#1DAB6F'; // Thay #ff0000 bằng mã màu của bạn
+  }
+}
+function settime(time, publishedDate) {
+  let currentDate = new Date();
+  let query = time.split('|')[0];
+  let timepoint = new Date();
 
+  switch (query) {
+    case 'd':
+      timepoint.setDay(currentDate.getDay() - parseInt(time.split('|')[1]));
+      return publishedDate >= timepoint
+    case 'm':
+      timepoint.setMonth(currentDate.getMonth() - parseInt(time.split('|')[1]));
+      return publishedDate >= timepoint
+    case 'y':
+      timepoint.setFullYear(currentDate.getFullYear() - parseInt(time.split('|')[1]));
+      return publishedDate >= timepoint
+    default:
+      break;
+  }
+  return false;
 }
 
-async function fistrun(params) {
-  var secondaryNode = document.querySelector('#secondary-inner');
-  var listvideo = secondaryNode.querySelectorAll('ytd-compact-video-renderer');
+async function fistRunWatch() {
+  let secondaryNode = document.querySelector('#secondary-inner');
+  let listvideo = secondaryNode.querySelectorAll('ytd-compact-video-renderer');
   listvideo.forEach(async (element) => {
-    var videoid = element.querySelector('.yt-simple-endpoint.style-scope.ytd-compact-video-renderer').href.split('v=')[1];
-    Listurl.push(videoid);
-    var { viewCount, likeCount, published, channelId } = await videoinfo(videoid);
-    let publishedDate = new Date(published);
-    var channelsub = await getChannelSubscriberCount(channelId);
-    let currentDate = new Date();
-    let uploadDate = publishedDate.getDate() + "/" + (publishedDate.getMonth() + 1) + "/" + publishedDate.getFullYear();
-    var channelname = element.querySelector('[class=*"ytd-channel-name"]')
-    element.querySelector('[class=*"ytd-channel-name"]').textContent = channelId + "-> Sub: " + channelsub;
-    
-
-  });
-
-
-}
-function reloadchange(params) {
-
-}
-// Lấy element cần theo dõi
-var targetNode = document.querySelector('#secondary-inner');
-
-// Tạo một observer instance
-var observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    if (mutation.type === 'childList') {
-      reloadchange();
+    eleconfig = {
+      videoid: '.yt-simple-endpoint.style-scope.ytd-compact-video-renderer',
+      metadataline: '#metadata-line',
+      view: '.inline-metadata-item.ytd-video-meta-block',
+      videoTitle: '#video-title'
     }
+    await infovideo(element, time = 'm|6', eleconfig);
   });
-});
+}
 
-// Cấu hình observer: chỉ theo dõi thay đổi childList
-var config = { childList: true };
+async function fistRunSearch() {
+  let secondaryNode = document.querySelector('ytd-search');
+  let listvideo = secondaryNode.querySelectorAll('ytd-video-renderer');
+  listvideo.forEach(async (element) => {
+    let videoid = element.querySelector('a#video-title').getAttribute('href').split('v=')[1].split('&')[0];
+    eleconfig = {
+      videoid: videoid,
+      metadataline: '#metadata-line',
+      view: '.inline-metadata-item.ytd-video-meta-block',
+      videoTitle: 'a#video-title'
+    }
+    await infovideo(element, time = 'm|6', eleconfig);
+  });
+}
 
-// Bắt đầu quá trình theo dõi
-observer.observe(targetNode, config);
+function mutationSv(partennode, childnode) {
+  // Lấy element cần theo dõi
+  let targetNode = document.querySelector(partennode);
+
+  // Tạo một observer instance
+  let observer = new MutationObserver(function (mutations) {
+    mutations.forEach(mutation => {
+      if (mutation.type === 'childList') {
+        let addedNodes = mutation.addedNodes;
+        addedNodes.forEach(node => {
+          if (node.nodeName.toLowerCase() === childnode) {
+            //console.log('Node ytd-compact-video-renderer được thêm vào:', node);
+            infovideo(node);
+          }
+        });
+      }
+    });
+  });
+  // Cấu hình observer: chỉ theo dõi thay đổi childList
+  let config = { childList: true, subtree: true };
+  // Bắt đầu quá trình theo dõi
+  observer.observe(targetNode, config);
+}
+if (window.location.href.includes('watch?v')) {
+  mutationSv('#secondary-inner', 'ytd-compact-video-renderer')
+  await fistRunWatch();
+} else if (window.location.href.includes('results?search_query=')) {
+  mutationSv('ytd-search', 'ytd-video-renderer')
+  await fistRunSearch();
+}
+
